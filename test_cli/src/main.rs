0fa7ef57241051068e32cba921e01673a51e9ce5
fanjx <jack.h.b.fan@outlook.com>
2025-04-13 21:21:07 +0800

    练习异步开发

--- test_cli/src/main.rs
+++ test_cli/src/main.rs
@@ -11,10 +11,31 @@ fn main() -> anyhow::Result<()> {
                 output.clone()
             } else {
                 // format!("{}{}","output.",String::from(opts.format))
-                format!("{}{}","output.",Into::<&str>::into(opts.format))
+                format!("{}{}", "output.", Into::<&str>::into(opts.format))
             };
             process_csv(&opts.input, output, opts.format)?;
         }
     }
     Ok(())
 }
+
+// 可以接收 String 和 &str 类型的参数
+fn process_text<T: AsRef<str>>(text: T) {
+    let text_str = text.as_ref();
+    println!("Processing text: {}", text_str);
+}
+
+fn process_txt(txt: &str) {
+    println!("Processing text: {}", txt);
+}
+
+// fn main() {
+//     let string = String::from("Hello, Rust!");
+//     let str_literal = "Hello, World!";
+//
+//     // process_text(string); // 传递 String
+//     // process_text(str_literal); // 传递 &str
+//
+//     process_txt(&string);
+//     process_txt(str_literal);
+// }
