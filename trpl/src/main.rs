5699ff051e9ce9f974af2ea4ef5d4d9cddb97fd9
fanjx <jack.h.b.fan@outlook.com>
2025-04-20 21:08:55 +0800

    练习std

---
+++ trpl/src/main.rs
@@ -0,0 +1,47 @@
+use futures::channel::mpsc;
+use futures::{SinkExt, StreamExt};
+use std::thread;
+use std::thread::Thread;
+use std::time::Duration;
+
+#[tokio::main]
+async fn main() {
+    blocks().await;
+    let string = format!("{name} {}", 1, name = 2);
+    println!("{}", string);
+    let string1 = format!("{1} {} {0} {}", 1, 3); // => "3 1 1 3"
+    println!("{}", string1);
+    println!("Hello, world!");
+}
+// 多个不同的 `async` 语句块可以访问同一个本地变量，只要它们在该变量的作用域内执行
+async fn blocks() {
+    let my_string = "foo".to_string();
+
+    let future_one = async {
+        // ...
+        println!("{my_string}");
+    };
+
+    let future_two = async {
+        // ...
+        println!("{my_string}");
+    };
+
+    // 运行两个 Future 直到完成
+    let ((), ()) = futures::join!(future_one, future_two);
+}
+
+async fn send_recv() {
+    const BUFFER_SIZE: usize = 10;
+    let (mut tx, mut rx) = mpsc::channel::<i32>(BUFFER_SIZE);
+
+    tx.send(1).await.unwrap();
+    tx.send(2).await.unwrap();
+    drop(tx);
+
+    // `StreamExt::next` 类似于 `Iterator::next`, 但是前者返回的不是值，而是一个 `Future<Output = Option<T>>`，
+    // 因此还需要使用`.await`来获取具体的值
+    assert_eq!(Some(1), rx.next().await);
+    assert_eq!(Some(2), rx.next().await);
+    assert_eq!(None, rx.next().await);
+}
